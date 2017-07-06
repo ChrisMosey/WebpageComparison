ffd56b3fb85ba76d264098e92516e71f93cfd132
Chris Mosey <chris.c.mosey@gmail.com>
2017-07-06 13:27:37 -0400

    this only works on linux so this is an easy way to move it back and fourth

--- app.js
+++ app.js
@@ -2,12 +2,25 @@ const chrome = require('chrome-remote-interface');
 const argv = require('minimist')(process.argv.slice(2));
 const fs = require('fs');
 
-const targetURL = argv.url || 'https://jonathanmh.com';
+let targets = argv.url || require("./websites.json");
+const full = argv.full || false;
+
 const viewport = [1440, 900];
 const screenshotDelay = 2000; // ms
-const fullPage = argv.fullPage || false;
 
-if (fullPage) {
+if (typeof(targets) != "array") {
+    let type = "first page";
+    if (full) {
+        type = "full";
+    }
+
+    targets = [{
+        url: targets,
+        captureType: type
+    }];
+}
+
+if (full) {
     console.log("will capture full page")
 }
 
@@ -32,36 +45,39 @@ chrome(async(client) => {
     await Emulation.setDeviceMetricsOverride(device);
     await Emulation.setVisibleSize({ width: viewport[0], height: viewport[1] });
 
-    await Page.navigate({ url: targetURL });
+    targets.forEach((target) => {
+        await Page.navigate({ url: target });
 
-    Page.loadEventFired(async() => {
-        if (fullPage) {
-            const { root: { nodeId: documentNodeId } } = await DOM.getDocument();
-            const { nodeId: bodyNodeId } = await DOM.querySelector({
-                selector: 'body',
-                nodeId: documentNodeId,
-            });
+        Page.loadEventFired(async() => {
+            if (fullPage) {
+                const { root: { nodeId: documentNodeId } } = await DOM.getDocument();
+                const { nodeId: bodyNodeId } = await DOM.querySelector({
+                    selector: 'body',
+                    nodeId: documentNodeId,
+                });
 
-            const { model: { height } } = await DOM.getBoxModel({ nodeId: bodyNodeId });
+                const { model: { height } } = await DOM.getBoxModel({ nodeId: bodyNodeId });
 
-            // extend the viewport to display complete page
-            await Emulation.setVisibleSize({ width: device.width, height: height });
-            await Emulation.forceViewport({ x: 0, y: 0, scale: 1 });
-        }
-    });
-
-    setTimeout(async() => {
-        const screenshot = await Page.captureScreenshot({ format: "png", fromSurface: true });
-        const buffer = new Buffer(screenshot.data, 'base64');
-        fs.writeFile('desktop.png', buffer, 'base64', (err) => {
-            if (err) {
-                console.error(err);
-            } else {
-                console.log('Screenshot saved');
+                // extend the viewport to display complete page
+                await Emulation.setVisibleSize({ width: device.width, height: height });
+                await Emulation.forceViewport({ x: 0, y: 0, scale: 1 });
             }
         });
-        client.close();
-    }, screenshotDelay);
+
+        setTimeout(async() => {
+            const screenshot = await Page.captureScreenshot({ format: "png", fromSurface: true });
+            const buffer = new Buffer(screenshot.data, 'base64');
+            fs.writeFile('desktop.png', buffer, 'base64', (err) => {
+                if (err) {
+                    console.error(err);
+                } else {
+                    console.log('Screenshot saved');
+                }
+            });
+            client.close();
+        }, screenshotDelay);
+
+    }, this);
 
 }).on('error', err => {
     console.error('Cannot connect to browser:', err);
